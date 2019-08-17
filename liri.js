6176946899b75d0770b788849065f3ebecb2a7d2
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-08-17 15:29:17 -0700

    created variables for require packages and search arguments

--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
-
+var fs = require("fs");
+var axios = require("axios");
 var keys = require("./keys.js");
-
 var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+
+var spotifyTest = process.env.SPOTIFY_ID
+var command = process.argv[2]
+var search = process.argv[3]
+
