cf0cc3db8bb44e71518918f643318cf87e8c1009
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-08-18 14:52:50 -0700

    switch cases added

--- liri.js
+++ liri.js
@@ -2,10 +2,29 @@ require("dotenv").config();
 var fs = require("fs");
 var axios = require("axios");
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: keys.id,
+    secret: keys.secret
+});
 
-var spotifyTest = process.env.SPOTIFY_ID
+
 var command = process.argv[2]
 var search = process.argv[3]
+
+
+switch (command) {
+    case "concert-this": 
+        bandsInTown(search);
+        break;
+    case "spotify-this-song":
+        spotifyThis(search);
+        break;
+    case "movie-this":
+        movieThis(search);
+        break;
+    case "do-what-it-says":
+        randomChoice();
+        break;
+}
 
