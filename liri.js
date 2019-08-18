591fd271d2d329d5fedba2ec4c9775f3a3fe6691
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-08-18 15:01:15 -0700

    getting response from bandsintown call

--- liri.js
+++ liri.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 var fs = require("fs");
 var axios = require("axios");
-var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify({
-    id: keys.id,
-    secret: keys.secret
-});
+// var keys = require("./keys.js");
+// var Spotify = require('node-spotify-api');
+// var spotify = new Spotify({
+//     id: keys.id,
+//     secret: keys.secret
+// });
 
 
 var command = process.argv[2]
@@ -17,9 +17,9 @@ switch (command) {
     case "concert-this": 
         bandsInTown(search);
         break;
-    case "spotify-this-song":
-        spotifyThis(search);
-        break;
+    // case "spotify-this-song":
+    //     spotifyThis(search);
+    //     break;
     case "movie-this":
         movieThis(search);
         break;
@@ -28,3 +28,23 @@ switch (command) {
         break;
 }
 
+function bandsInTown(artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryURL).then(
+        function(response) {
+            if(response.data[0].venue != undefined) {
+                console.log("----------------")
+                console.log("Event Venue: " + response.data[0].venue.name);
+                console.log("Event location: " + response.data[0].venue.city);
+                var eventDateTime = moment(response.data[0].datetime);
+                console.log("Event Date & Time: " + eventDateTime.format("dddd, MMMM Do YYYY"));
+                console.log("-----------------")
+            }
+            else {
+                console.log("We found no results.");
+            }
+        }
+    ).catch(function (error) {
+        console.log (error);
+    });
+}
